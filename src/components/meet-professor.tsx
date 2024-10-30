fdf56b9a210f5200e35a770a690e275dc0a31c1f
Sarmad rafique <sarmadrafique040@gmail.com>
2024-10-30 20:10:15 +0500

    Meet the professor component

--- src/components/meet-professor.tsx
+++ src/components/meet-professor.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const MeetProfessor = () => {
   return (
-    <div className="w-screen mx-7 my-8">
+    <div className="w-screen mx-7 my-8 bg-[#FAF5E2] text-black">
       <div className="grid grid-cols-3">
-        <div className="flex flex-col justify-between gap-y-12 col-span-1">
+        <div className="flex flex-col justify-between gap-y-12 col-span-1 p-3">
           <h3 className="text-4xl">Meet your professor</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
@@ -16,8 +16,8 @@ const MeetProfessor = () => {
         </div>
         <div className="col-span-2">
           <iframe
-            width="560"
-            height="315"
+            width="100%"
+            height="400"
             src="https://www.youtube.com/embed/j7W44OPRVgM?si=7IYWk19Msgm3DLHB"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
