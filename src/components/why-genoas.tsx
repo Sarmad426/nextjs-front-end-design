009c378fc8dc9846f0b1e1f09d5946a3fdb69100
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-20 19:35:41 +0500

    Why Genoa section component. Incomplete

---
+++ src/components/why-genoas.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import React from "react";
+
+export const WhyGenoas = () => {
+  return (
+    <div className="md:w-[95%] w-full">
+      <div className="p-8 w-[95%]">
+        <h4 className="text-start font-normal text-xl leading-6 md:text-2xl md:leading-8 text-primaryColor my-2">
+          Why Genoa Entrepreneurship School?
+        </h4>
+        <hr className="border-[#BCD4C7]" />
+        <hr className="border border-[#193E2C] lg:w-[250px] w-[220px] -mt-[.5px]" />
+      </div>
+      <div className="md:mx-32 sm:mx-20 mx-4 sm:text-center">
+        <h3 className="font-onest font-light text-[22px] leading-[26px] text-primaryColor sm:text-inherit md:text-[40px] md:leading-[48px]">
+          <span className="text-primaryColor">Learn and grow</span> with
+          personalized expert guidance in the world&apos;s most stimulating
+          cities while studying
+        </h3>
+      </div>
+      <div className="sm:mx-6 mx-3 lg:grid lg:grid-cols-3 mt-12 flex flex-col flex-wrap items-center justify-center md:flex md:flex-row gap-4 w-full">
+        <div className="col-span-1">
+          <div className="sm:border-l sm:border-primaryColor sm:pl-8 p-3">
+            <div className="flex md:flex-col font-onest gap-4">
+              <h4 className="text-primaryColor font-light text-2xl leading-[24px] md:text-[32px] md:leading-[32px]">
+                01
+              </h4>
+              <h3 className="font-light text-lg leading-[24px] md:text-xl lg:text-2xl">
+                Personalized guidance from the best startup experts for life,
+                even if you don&apos;t launch.
+              </h3>
+            </div>
+            <hr className="border-[#BCD4C7] w-[22rem] sm:hidden my-2" />{" "}
+            <ul className="p-2 list-disc font-light text-base leading-[20px] md:text-xl lg:text-lg lg:leading-[24px]">
+              <li className="my-1">
+                1:1 meetings at any time with field-specific experts and
+                founders.
+              </li>
+              <li>
+                A personal mentor will follow you throughout the year, every
+                week.
+              </li>
+              <li>
+                Daily workshops provide industry tips and adapt to your
+                full-time startup commitments.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="sm:border-l sm:border-primaryColor sm:pl-8 p-3">
+            <div className="flex md:flex-col font-onest gap-4">
+              <h4 className="text-primaryColor font-light text-2xl leading-[24px] md:text-[32px] md:leading-[32px]">
+                02
+              </h4>
+              <h3 className="font-light text-lg leading-[24px] md:text-xl lg:text-2xl">
+                Learn by combining theory and practice by building in the most
+                stimulating US & EU cities
+              </h3>
+            </div>
+            <hr className="border-[#BCD4C7] w-[22rem] sm:hidden my-2" />{" "}
+            <ul className="p-2 list-disc font-light text-base leading-[20px] md:text-xl lg:text-lg lg:leading-[24px]">
+              <li className="my-1">
+                1:1 meetings at any time with field-specific experts and
+                founders.
+              </li>
+              <li>
+                A personal mentor will follow you throughout the year, every
+                week.
+              </li>
+              <li>
+                Daily workshops provide industry tips and adapt to your
+                full-time startup commitments.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="sm:border-l sm:border-primaryColor sm:pl-8 p-3">
+            <div className="flex md:flex-col font-onest gap-4">
+              <h4 className="text-primaryColor font-light text-2xl leading-[24px] md:text-[32px] md:leading-[32px]">
+                03
+              </h4>
+              <h3 className="font-light text-lg leading-[24px] md:text-xl lg:text-2xl">
+                75% of our students closed a round within 6 months
+              </h3>
+            </div>
+            <hr className="border-[#BCD4C7] w-[22rem] sm:hidden my-2" />{" "}
+            <ul className="p-2 list-disc font-light text-base leading-[20px] md:text-xl lg:text-lg lg:leading-[24px]">
+              <li className="my-1">
+                1:1 meetings at any time with field-specific experts and
+                founders.
+              </li>
+              <li>
+                A personal mentor will follow you throughout the year, every
+                week.
+              </li>
+              <li>
+                Daily workshops provide industry tips and adapt to your
+                full-time startup commitments.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
