f0d8c6c663488db8241521eb3e778b8261e22de1
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-21 21:55:37 +0500

    Start journey component

---
+++ src/components/start-journey.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+import Journey from "@/assets/journey.webp";
+import { ArrowUpRight } from "lucide-react";
+
+const StartJourney = () => {
+  return (
+    <div className="mx-auto w-[95%] xl:grid xl:grid-cols-1">
+      <div className="flex items-center flex-col lg:flex-row mt-8 w-full xl:col-span-1 xl:justify-center">
+        <div className="flex items-center justify-between">
+          <Image
+            src={Journey}
+            alt="Image"
+            className="min-w-full h-[300px] md:w-[720px] md:h-[400px] lg:h-[500px]"
+          />
+        </div>
+        <div className="bg-[#193E2C] w-full p-6 md:w-[720px] lg:h-[500px]">
+          <div className="lg:flex lg:flex-col md:flex">
+            <h4 className="my-2 text-primaryColor font-onest font-normal text-lg leading-6 md:text-[32px] md:leading-10 lg:text-2xl">
+              Ready to start your journey
+            </h4>
+            <p className="font-light text-2xl md:text-[32px] md:leading-10 text-[#FCFAF2] my-2">
+              Genoa ES can help you achieve your academic, entrepreneurial, and
+              career goals.
+            </p>
+          </div>
+          <div className="lg:mt-20 mt-7">
+            <div className="flex items-center md:justify-end justify-center">
+              <button className="uppercase btn mt-4 flex items-center justify-center gap-3 bg-primaryColor hover:bg-hoverColor text-white text-lg py-2 w-[340px]">
+                Discover our Program
+                <ArrowUpRight className="w-7" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StartJourney;
