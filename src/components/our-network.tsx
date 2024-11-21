e642300697556b8d33f103c8baddbc80b7685e9c
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-21 07:41:24 +0500

    Made the design responsive

--- src/components/our-network.tsx
+++ src/components/our-network.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Mission from "@/assets/mission.webp";
+import MissionMobile from "@/assets/mission-mobile.webp";
 import Image from "next/image";
 import { ArrowRight } from "lucide-react";
 
@@ -15,20 +16,33 @@ const OurNetwork = () => {
         <hr className="border border-[#193E2C] lg:w-[250px] w-[220px] -mt-[.5px]" />
       </div>
       <div className="mx-6 flex items-center justify-center w-full relative">
-        <Image src={Mission} alt="Our mission" className="hidden sm:flex" />
-        <div className="absolute right-1 top-8 text-white w-[45%]">
-          <h3 className="font-onest font-light lg:text-5xl lg:leading-[54px]">
+        <Image src={Mission} alt="Our mission" className="hidden lg:flex" />
+        <Image
+          src={MissionMobile}
+          alt="Our mission"
+          // className="sm:hidden flex"
+          width="8000"
+          height="8000"
+          decoding="async"
+          data-nimg="1"
+          className="mt-[24px] lg:hidden flex md:max-h-[800px] object-cover object-bottom"
+          loading="lazy"
+          style={{ color: "transparent" }}
+        />
+
+        <div className="absolute right-1 top-8 text-white lg:w-[45%] w-full p-3.5">
+          <h3 className="font-onest font-light text-[32px] leading-[32px] md:text-[40px] md:leading-[48px] lg:text-5xl lg:leading-[54px]">
             Joined by experts who{" "}
             <span className="text-primaryColor">raised</span> over{" "}
             <span className="text-primaryColor">$7.5 billion</span> and manage{" "}
             <span className="text-primaryColor">$100 billion</span> in assets
             combined
           </h3>
-          <h5 className="font-light lg:text-2xl text-[#FCFAF2]">
+          <h5 className="font-light text-xl leading-[26px] md:text-2xl text-[#FCFAF2] my-3">
             World experts dedicate their time to help our students grow.
           </h5>
-          <div className="flex items-center md:justify-end justify-center mt-10 mx-6">
-            <button className="md:w-[250px] md:h-[52px] w-[330px] h-[48px] btn bg-transparent border border-white hover:bg-white hover:text-black flex items-center justify-center gap-2">
+          <div className="flex items-center md:justify-end justify-end mt-10 mx-6">
+            <button className="w-[237px] h-[48px] md:w-[269px] md:h-[52px] btn bg-transparent border border-white hover:bg-white hover:text-black flex items-center justify-center gap-2">
               Discover the network
               <ArrowRight className="w-4" />
             </button>
