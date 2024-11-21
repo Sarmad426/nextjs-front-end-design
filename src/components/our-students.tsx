c55d71f34cfefa0ad301d7a27462b89e5bd35f5f
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-21 18:03:54 +0500

    Finished the card design

--- src/components/our-students.tsx
+++ src/components/our-students.tsx
@@ -18,13 +18,13 @@ const OurStudents = () => {
         <ArrowDown size={35} className="text-primaryColor flex md:hidden" />
       </div>
       <div className="col-span-3 flex gap-4 overflow-x-scroll" dir="ltr">
-        <div className="bg-[#E5EBDD] p-4 w-[300px] h-[600px] overflow-hidden">
+        <div className="bg-[#E5EBDD] p-4 w-[300px] h-[600px]">
           <p className="font-light text-xl leading-6 md:text-2xl">
             “I wasn&apos;t considering pursuing a master&apos;s degree but this
             course is practical and opens incredible doors. It certainly is the
             experience that has improved me the most in my life.”
           </p>
-          <div className="mt-4">
+          <div className="mt-12 mb-2">
             <Image src={Linkedin} alt="linkedin" />
             <h5 className="font-medium text-base leading-5 my-3">Lea Delic</h5>
             <p className="font-light text-sm md:text-lg leading-6">
@@ -34,26 +34,7 @@ const OurStudents = () => {
           <Image
             src={LiaDelic}
             alt="lia delic"
-            className="md:w-[90%] w-[95%]"
-          />
-        </div>
-        <div className="bg-[#E5EBDD] p-4 w-[300px] h-[600px] overflow-hidden">
-          <p className="font-light text-xl leading-6 md:text-2xl">
-            “I wasn&apos;t considering pursuing a master&apos;s degree but this
-            course is practical and opens incredible doors. It certainly is the
-            experience that has improved me the most in my life.”
-          </p>
-          <div className="mt-4">
-            <Image src={Linkedin} alt="linkedin" />
-            <h5 className="font-medium text-base leading-5 my-3">Lea Delic</h5>
-            <p className="font-light text-sm md:text-lg leading-6">
-              Student founder 2023
-            </p>
-          </div>
-          <Image
-            src={LiaDelic}
-            alt="lia delic"
-            className="md:w-[90%] w-[95%]"
+            className="md:w-[90%] w-[95%] mx-auto"
           />
         </div>
       </div>
