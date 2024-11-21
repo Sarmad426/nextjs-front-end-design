f4288b7c61ca917d525fc9fb914a0c1abb86cccc
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-21 18:57:48 +0500

    Fixed cards layout issues

--- src/components/our-students.tsx
+++ src/components/our-students.tsx
@@ -5,11 +5,12 @@ import Linkedin from "@/assets/linkedin.svg";
 import Image from "next/image";
 
 import LiaDelic from "@/assets/lea-delic.webp";
-// import Link from "next/link";
+import Chalermchon from "@/assets/chalermchon.webp";
+import GabrielRossi from "@/assets/gabriele-rossi.webp";
 
 const OurStudents = () => {
   return (
-    <div className="flex flex-col lg:grid lg:grid-cols-4 gap-4 my-4 mx-6">
+    <div className="flex flex-col lg:grid grid-cols-4 gap-4 my-4 md:mx-6 mx-1.5">
       <div className="col-span-1 flex lg:flex-col justify-between">
         <h3 className="font-onest font-light text-[22px] leading-[26px] md:text-[32px] md:leading-10 lg:w-full md:w-[75%] w-[90%]">
           Understand the Experience Directly From our students
@@ -17,8 +18,8 @@ const OurStudents = () => {
         <ArrowRight size={50} className="text-primaryColor hidden md:flex" />
         <ArrowDown size={35} className="text-primaryColor flex md:hidden" />
       </div>
-      <div className="col-span-3 flex gap-4 overflow-x-scroll" dir="ltr">
-        <div className="bg-[#E5EBDD] p-4 w-[300px] h-[600px]">
+      <div className="col-span-3 flex gap-2.5 overflow-x-scroll w-full">
+        <div className="bg-[#E5EBDD] p-4 min-w-[300px] min-h-[600px] md:h-[640px] md:min-w-[436px] overflow-hidden">
           <p className="font-light text-xl leading-6 md:text-2xl">
             “I wasn&apos;t considering pursuing a master&apos;s degree but this
             course is practical and opens incredible doors. It certainly is the
@@ -34,7 +35,49 @@ const OurStudents = () => {
           <Image
             src={LiaDelic}
             alt="lia delic"
-            className="md:w-[90%] w-[95%] mx-auto"
+            className="h-[250px] lg:h-[229px] xl:h-[450px] w-full object-cover object-top"
+          />
+        </div>
+        <div className="bg-[#E5EBDD] p-4 min-w-[300px] min-h-[600px] md:h-[640px] md:min-w-[436px] overflow-hidden">
+          <p className="font-light text-xl leading-6 md:text-2xl">
+            “The other students are amazing people, and mentors are incredible.
+            They are at a global level, but they take the time to be with you
+            and are ready to support you 24/7.”
+          </p>
+          <div className="mt-12 mb-2">
+            <Image src={Linkedin} alt="linkedin" />
+            <h5 className="font-medium text-base leading-5 my-3">
+              Chalermchon Puapolthep
+            </h5>
+            <p className="font-light text-sm md:text-lg leading-6">
+              Student founder 2023
+            </p>
+          </div>
+          <Image
+            src={Chalermchon}
+            alt="Chalermchon"
+            className="h-[250px] lg:h-[229px] xl:h-[450px] w-full object-cover object-top"
+          />
+        </div>
+        <div className="bg-[#E5EBDD] p-4 min-w-[300px] min-h-[600px] md:h-[640px] md:min-w-[436px] overflow-hidden">
+          <p className="font-light text-xl leading-6 md:text-2xl">
+            “The other students are amazing people, and mentors are incredible.
+            They are at a global level, but they take the time to be with you
+            and are ready to support you 24/7.”
+          </p>
+          <div className="mt-12 mb-2">
+            <Image src={Linkedin} alt="linkedin" />
+            <h5 className="font-medium text-base leading-5 my-3">
+              Chalermchon Puapolthep
+            </h5>
+            <p className="font-light text-sm md:text-lg leading-6">
+              Student founder 2023
+            </p>
+          </div>
+          <Image
+            src={GabrielRossi}
+            alt="Gabriel"
+            className="h-[250px] lg:h-[229px] xl:h-[450px] w-full object-cover object-top"
           />
         </div>
       </div>
