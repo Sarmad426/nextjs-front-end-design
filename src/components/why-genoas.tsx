7afcccb84eb5fb44885c346fb755510404365f4b
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-22 07:59:34 +0500

    Fixed potential responsiveness issues

--- src/components/why-genoas.tsx
+++ src/components/why-genoas.tsx
@@ -4,16 +4,16 @@ import React from "react";
 
 export const WhyGenoas = () => {
   return (
-    <div className="md:w-[95%] w-full flex flex-col items-center justify-center xl:mx-16">
-      <div className="p-8 w-[95%] xl:w-full">
+    <div className="md:w-[95%] mx-auto w-full flex flex-col items-center justify-center xl:mx-16">
+      <div className="p-8 w-[95%] md:w-full xl:w-full">
         <h4 className="text-start font-normal text-xl leading-6 md:text-2xl md:leading-8 text-primaryColor my-2">
           Why Genoa Entrepreneurship School?
         </h4>
         <hr className="border-[#BCD4C7]" />
         <hr className="border border-[#193E2C] lg:w-[250px] w-[220px] -mt-[.5px]" />
       </div>
-      <div className="md:mx-32 sm:mx-20 mx-4 sm:text-center">
-        <h3 className="font-onest font-light text-[22px] leading-[26px] text-primaryColor sm:text-inherit md:text-[40px] md:leading-[48px]">
+      <div className="lg:mx-32 md:mx-16 sm:mx-20 mx-4 md:text-center">
+        <h3 className="font-onest font-light text-[22px] leading-[26px] text-primaryColor md:text-inherit md:text-[40px] md:leading-[48px]">
           <span className="text-primaryColor">Learn and grow</span> with
           personalized expert guidance in the world&apos;s most stimulating
           cities while studying
@@ -21,7 +21,7 @@ export const WhyGenoas = () => {
       </div>
       <div className="sm:mx-5 mx-3 md:grid lg:grid-cols-3 md:grid-cols-2 my-6 flex flex-col gap-4 items-start justify-start w-full">
         <div className="relative col-span-1 mx-2">
-          <div className="absolute -left-3 top-0 border-l border-primaryColor h-full rotate-180 hidden sm:flex" />
+          <div className="absolute -left-3 top-0 border-l border-primaryColor h-full rotate-180 hidden md:flex" />
           <div className="sm:pl-5 p-3">
             <div className="flex md:flex-col items-start justify-start font-onest gap-4">
               <h4 className="text-primaryColor font-light text-2xl leading-[24px] md:text-[32px] md:leading-[32px]">
@@ -32,7 +32,7 @@ export const WhyGenoas = () => {
                 even if you don&apos;t launch.
               </h3>
             </div>
-            <hr className="border-[#BCD4C7] w-[22rem] sm:hidden my-2" />{" "}
+            <hr className="border-[#BCD4C7] w-[22rem] md:hidden my-2" />{" "}
             <ul className="p-2 list-disc font-light text-base leading-[20px] md:text-xl lg:text-lg lg:leading-[24px]">
               <li className="my-1">
                 1:1 meetings at any time with field-specific experts and
@@ -61,7 +61,7 @@ export const WhyGenoas = () => {
                 stimulating US & EU cities{" "}
               </h3>
             </div>
-            <hr className="border-[#BCD4C7] w-[22rem] sm:hidden my-2" />{" "}
+            <hr className="border-[#BCD4C7] w-[22rem] md:hidden my-2" />{" "}
             <ul className="p-2 list-disc font-light text-base leading-[20px] md:text-xl lg:text-lg lg:leading-[24px]">
               <li className="my-1">
                 1:1 meetings at any time with field-specific experts and
@@ -89,7 +89,7 @@ export const WhyGenoas = () => {
                 75% of our students closed a round within 6 months
               </h3>
             </div>
-            <hr className="border-[#BCD4C7] w-[22rem] sm:hidden my-2" />{" "}
+            <hr className="border-[#BCD4C7] w-[22rem] md:hidden my-2" />{" "}
             <ul className="p-2 list-disc font-light text-base leading-[20px] md:text-xl lg:text-lg lg:leading-[24px]">
               <li className="my-1">
                 1:1 meetings at any time with field-specific experts and
