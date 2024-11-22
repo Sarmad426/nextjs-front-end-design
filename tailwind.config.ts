cd48925dc44a480527d6da0e8da5b1a20f2ff7d9
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-22 07:59:19 +0500

    Updated screen styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,21 @@ const config: Config = {
   ],
   theme: {
     screens: {
-      md: '744px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '744px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
     },
     extend: {
       colors: {
