f5a8eecc9f95fbf849dd90154f090287fc9160af
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-22 15:45:46 +0500

    About page

---
+++ src/app/(routes)/about/page.tsx
@@ -0,0 +1,12 @@
+import { AboutHero } from "@/components/about/about-hero";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="w-full mb-[90%]">
+      <AboutHero />
+    </div>
+  );
+};
+
+export default About;
