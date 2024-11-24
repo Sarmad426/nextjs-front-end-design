4eceb568e3d543a6b430b0d214dde1db422ba7fa
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-24 17:45:36 +0500

    Added data arrays in separate module

---
+++ src/data/data.ts
@@ -0,0 +1,77 @@
+import GoogleIcon from "@/assets/google.svg";
+import AppleIcon from "@/assets/apple.svg";
+import MetaIcon from "@/assets/meta.svg";
+import TeslaIcon from "@/assets/tesla.svg";
+import MicrosoftIcon from "@/assets/microsoft.svg";
+import SequoiaIcon from "@/assets/sequoia.svg";
+import IbmIcon from "@/assets/ibm.svg";
+import GrammarlyIcon from "@/assets/grammarly.svg";
+import SkypeIcon from "@/assets/skype.svg";
+import Skyscanner from "@/assets/skyscanner.svg";
+import PinterestIcon from "@/assets/pinterest.svg";
+import TalisIcon from "@/assets/talis.svg";
+import YounitedIcon from "@/assets/younited.svg";
+
+
+import GrammarlyLightIcon from "@/assets/grammarly-light.svg";
+import GoogleLightIcon from "@/assets/google-light.svg";
+import SequoiaLightIcon from "@/assets/sequoia-light.svg";
+import RipplingIcon from "@/assets/rippling.svg";
+import YounitedLightIcon from "@/assets/younited-light.svg";
+
+
+import T1 from "@/assets/t1.webp";
+import T2 from "@/assets/t2.webp";
+import T3 from "@/assets/t3.webp";
+import T4 from "@/assets/t4.webp";
+import T5 from "@/assets/t5.webp";
+
+
+export const icons = [
+    GoogleIcon,
+    AppleIcon,
+    MetaIcon,
+    TeslaIcon,
+    MicrosoftIcon,
+    SequoiaIcon,
+    IbmIcon,
+    GrammarlyIcon,
+    SkypeIcon,
+    Skyscanner,
+    PinterestIcon,
+    TalisIcon,
+    YounitedIcon,
+];
+
+export const experts = [
+    {
+        img: T1,
+        orgIcon: GrammarlyLightIcon,
+        name: "Bei Zhang",
+        intro: "Head of Marketing Analytics at Grammarly ($13 bln valuation)",
+    },
+    {
+        img: T2,
+        orgIcon: GoogleLightIcon,
+        name: "Alessandro Cannas",
+        intro: "Product Manager at Gemini (Google) (1.65 trillion valuation)",
+    },
+    {
+        img: T3,
+        orgIcon: SequoiaLightIcon,
+        name: "Douglas Leone",
+        intro: "Global Managing Partner at Sequoia ($88 bln under m.)",
+    },
+    {
+        img: T4,
+        orgIcon: RipplingIcon,
+        name: "Prasanna Sankar",
+        intro: "Co-Founder at Rippling.com ($11.3 bln valuation)",
+    },
+    {
+        img: T5,
+        orgIcon: YounitedLightIcon,
+        name: "Francois de Bodinat",
+        intro: "Chief Product Officer at Younited ($1.3 bln valuation)",
+    },
+];
