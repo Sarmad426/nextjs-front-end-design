e3cd4f8d63eeef6669d2f323120329b9c55db949
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-25 20:06:02 +0500

    Improved responsive design for mobile screens

--- src/components/about/our-believe.tsx
+++ src/components/about/our-believe.tsx
@@ -1,10 +1,10 @@
 export const OurBelieve = () => {
   const cardsDivStyles =
     "flex flex-col lg:flex-row gap-[16px] w-full px-[16px] lg:px-[40px] h-full lg:h-[400px] xl:h-[360px]";
-    return (
-    <section className="w-[95%] mx-auto my-12">
+  return (
+    <section className="w-full md:w-[95%] mx-auto my-12">
       <div>
-        <div className="flex flex-col gap-4 lg:flex-row lg:justify-evenly items-start lg:items-center my-6">
+        <div className="flex flex-col gap-4 lg:flex-row lg:justify-evenly items-start lg:items-center mx-4 my-6">
           <h2 className="font-light text-[32px] leading-10 md:text-[56px] md:leading-[64px]">
             What we believe in
           </h2>
@@ -17,12 +17,12 @@ export const OurBelieve = () => {
           </p>
         </div>
         <div className={cardsDivStyles}>
-          <div className="flex flex-col w-full lg:max-w-[48%] bg-[#FAF5E2] p-[16px] md:p-[40px] h-max lg:h-[360px] gap-[20px] justify-between relative group">
+          <div className="flex flex-col lg:max-w-[48%] bg-[#FAF5E2] p-3 md:p-[40px] w-full h-[330px] lg:h-[360px] gap-[20px] justify-between relative group">
             <div className="absolute left-0 top-0 bottom-0 w-[4px] h-full bg-[#43AA13] opacity-0 transition-all duration-[.3s] group-hover:opacity-100" />
             <h3 className="text-[24px] lg:text-[32px] font-500 uppercase text-[#193E2C]">
               Global Approach
             </h3>
-            <p className="text-[18px] leading-[24px] text-[#202020b3] font-300">
+            <p className="md:text-[18px] leading-[24px] text-[#202020b3] font-300">
               Scaling across the Atlantic is challenging, as evidenced by Uber
               and various European ventures. We believe learning from top US and
               EU institutions and mentors at the early stage of an idea greatly
@@ -31,7 +31,7 @@ export const OurBelieve = () => {
               network for successful growth right from the start.
             </p>
           </div>
-          <div className="flex flex-col w-full lg:max-w-[48%] bg-[#FAF5E2] p-[16px] md:p-[40px] h-max lg:h-[360px] gap-[20px] justify-between relative group">
+          <div className="flex flex-col w-full lg:max-w-[48%] bg-[#FAF5E2] p-3 md:p-[40px] h-min lg:h-[360px] gap-[20px] justify-between relative group">
             <div className="absolute left-0 top-0 bottom-0 w-[4px] h-full bg-[#43AA13] opacity-0 transition-all duration-[.3s] group-hover:opacity-100" />
             <h3 className=" text-[24px] lg:text-[32px] font-500 uppercase text-[#193E2C]">
               Personal Mentorship
@@ -50,7 +50,7 @@ export const OurBelieve = () => {
         <div className={`${cardsDivStyles} justify-end mt-[16px]`}>
           <div className="flex flex-col w-full lg:max-w-[48%] bg-[#FAF5E2] p-[16px] h-max lg:h-[360px] md:p-[40px] gap-[20px] justify-between relative group">
             <div className="absolute left-0 top-0 bottom-0 w-[4px] h-full bg-[#43AA13] opacity-0 transition-all duration-[.3s] group-hover:opacity-100" />
-            <h3 className=" text-[24px] lg:text-[32px] font-500 uppercase text-[#193E2C]">
+            <h3 className="text-[24px] lg:text-[32px] font-500 uppercase text-[#193E2C]">
               Real World experience
             </h3>
             <p className="text-[18px] leading-[24px] text-[#202020b3] font-300">
@@ -79,5 +79,5 @@ export const OurBelieve = () => {
         </div>
       </div>
     </section>
-)
+  );
 };
