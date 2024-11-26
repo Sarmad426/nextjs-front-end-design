e3926824a328d13f71d44b027f9463c01faad0b8
Sarmad rafique <sarmadrafique040@gmail.com>
2024-11-26 11:47:58 +0500

    Programs page

---
+++ src/app/(routes)/programs/page.tsx
@@ -0,0 +1,12 @@
+import { ProgramsHero } from "@/components/programs/programs-hero";
+import React from "react";
+
+const Programs = () => {
+  return (
+    <div className="min-h-screen w-full">
+      <ProgramsHero />
+    </div>
+  );
+};
+
+export default Programs;
