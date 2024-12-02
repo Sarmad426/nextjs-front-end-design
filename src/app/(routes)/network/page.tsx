14cc911881f7f3e331a29095599ec4d0c2e5c0ec
Sarmad rafique <sarmadrafique040@gmail.com>
2024-12-02 13:50:51 +0500

    Network page

---
+++ src/app/(routes)/network/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NetworkHero } from "@/components/network/network-hero";
+
+const NetworkPage = () => {
+  return (
+    <main className="w-full my-6">
+      <NetworkHero />
+    </main>
+  );
+};
+
+export default NetworkPage;
